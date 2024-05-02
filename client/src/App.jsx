bde2631ed02cf0f356bb1bbe6dae2de7ea01c532
Ivin <ivinmk2410@hotmail.com>
2024-05-02 23:01:45 +0530

    Basic navbar and footer created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
-
-
+import { NavbarSimple } from "./components/navbar"
+import Footer from "./components/footer"
 
 function App() {
 
   return (
     <>
+      <NavbarSimple />
+      <div className="h-[100rem] bg-slate-400 text-white">
+        Hello
+      </div>
+      <Footer />
     </>
   )
 }
---
+++ client/src/components/navbar.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+  Navbar,
+  Collapse,
+  Typography,
+  IconButton,
+} from "@material-tailwind/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+ 
+function NavList() {
+  return (
+    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-24">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          About
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          Gallery
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          Location
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a href="#" className="flex items-center hover:text-blue-500 transition-colors">
+          Contact
+        </a>
+      </Typography>
+    </ul>
+  );
+}
+ 
+export function NavbarSimple() {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  const handleWindowResize = () =>
+    window.innerWidth >= 960 && setOpenNav(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+ 
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+ 
+  return (
+    <Navbar className="mx-auto w-full px-6 py-3 bg-green-600 text-black font-serif sticky top-0">
+      <div className="flex items-center justify-between text-blue-gray-900 mx-32 h-16">
+        <Typography
+          as="a"
+          href="#"
+          variant="h6"
+          className="mr-4 cursor-pointer py-1.5 font-extrabold text-xl"
+        >
+          Ezhuvelil Gardens
+        </Typography>
+        <div className="hidden lg:block">
+          <NavList />
+        </div>
+        <IconButton
+          variant="text"
+          className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+          ripple={false}
+          onClick={() => setOpenNav(!openNav)}
+        >
+            <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+          
+        </IconButton>
+      </div>
+      {/* <Collapse open={openNav}>
+        <NavList />
+      </Collapse> */}
+    </Navbar>
+  );
+}
